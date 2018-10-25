3b78c9fa0f0732564d3549a3a5df33c9e9cc4038
Arslan <arslan.gait@nu.edu.kz>
2018-10-26 01:15:03 +0600

    added serializable

--- src/main/java/kz/edu/nu/cs/Services/EventService.java
+++ src/main/java/kz/edu/nu/cs/Services/EventService.java
@@ -10,13 +10,14 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.Serializable;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.List;
 
 @Path("/event")
-public class EventService {
+public class EventService implements Serializable {
     private static final long serialVersionUID = 12252632212171L;
 
     @POST
--- src/main/java/kz/edu/nu/cs/Services/MainService.java
+++ src/main/java/kz/edu/nu/cs/Services/MainService.java
@@ -3,9 +3,10 @@ package kz.edu.nu.cs.Services;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.io.Serializable;
 
 @Path("/main")
-public class MainService{
+public class MainService implements Serializable {
 	private static final long serialVersionUID = 123654478955211411L;
 	
 	@GET
